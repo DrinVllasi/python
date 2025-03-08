919a453c1ebdc6c5e5ba20ffba7d9fda1f53825b
drin <vllasidrin@gmail.com>
2025-03-08 03:44:04 -0800

    ...

--- module16/challange.py
+++ module16/challange.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
-def calculate():
-
-
+def calculate(Pesha, Gjatesia):
+    bmi = Pesha / (Gjatesia ** 2)
+    return bmi
 
 def main():
 
     Emri = st.text_input("Enter your name")
     Mbiemri = st.text_input("Enter your last name")
-    Mosha = st.number_input("Enter your age",step=2)
-    Weight = st.number_input("Enter your weight in kg",)
-    Gjatesia = st.number_input("Enter your height in cm",step=2)
+    Mosha = st.number_input("Enter your age", step=2)
+    Pesha = st.number_input("Enter your weight in kg")
+    Gjatesia = st.number_input("Enter your height in m")
 
     def on_button_click():
-
-        st.write(f"{Emri} {Mbiemri} is {Mosha} years old and the BMI is ")
+        bmi = calculate(Pesha, Gjatesia)
+        st.write(f"{Emri} {Mbiemri} is {Mosha} years old and the BMI is {bmi:.2f}")
 
     if st.button("Press me"):
         on_button_click()
