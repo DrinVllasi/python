a7d661c3b252621780f2ee8de30998190eae6d31
drin <vllasidrin@gmail.com>
2025-05-10 03:42:52 -0700

    ...

--- Perseritje/main.py
+++ Perseritje/main.py
@@ -29,7 +29,7 @@ class Pet:
             print("Woof woof")
         elif(self.species == "cat"):
             print("Meow")
-        else: print(f"Its a {species}")
+        else: print(f"Its a {self.species}")
 
     def sleep(self):
         print(f"{self.name} is sleeping")
@@ -40,3 +40,54 @@ info = Pet("Riki",3,"dog","doberman","black")
 info.make_sound()
 info.sleep()
 info.eat()
+
+class Student:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        self._age = age
+
+infos = Student("Jimmy", 15)
+print(infos.get_name())
+print(infos.get_age())
+
+infos.set_name("John")
+print(infos.get_name())
+
+infos.set_age(18)
+print(infos.get_age())
+
+class Animal:
+    def sound(self):
+        print("Make a sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Ham ham")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow meow")
+
+animal = Animal()
+animal.sound()
+
+dog = Dog()
+dog.sound()
+
+cat = Cat()
+cat.sound()
+
+
+
