1da84a5a84a4c94e4b5ad4ec770493df90c24a24
drin <vllasidrin@gmail.com>
2025-09-20 12:45:24 +0200

    ...

--- Module24/main.py
+++ Module24/main.py
@@ -13,5 +13,33 @@ cursor.execute('''
     salary REAL
 )'''
 )
+connection.commit()
+
+cursor.execute('''
+        INSERT INTO employees(name,position,department,salary)
+        values(?,?,?,?)
+    ''', ('john','Software Developer', 'IT', 70000.00)
+)
+connection.commit()
+
+cursor.execute('SELECT * FROM employees')
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.execute('''
+UPDATE employees SET salary = ?
+WHERE name = ?
+''',(80000.00,'john')
+)
+connection.commit()
+
+cursor.execute('''
+DELETE FROM employees WHERE id = ?
+''',(1,))
 
 connection.commit()
+cursor.close()
+connection.close()
