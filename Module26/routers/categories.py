755600082f149ec8862813a3b56339a6a9688f87
drin <vllasidrin@gmail.com>
2025-11-08 12:47:33 +0100

    ...

--- Module26/routers/categories.py
+++ Module26/routers/categories.py
@@ -5,22 +5,22 @@ from models.category import Category, CategoryCreate
 from database import get_db_connection
 from fastapi import APIRouter, HTTPException
 
-
 router = APIRouter()
 
-@router.get("/categories/", response_model = List[Category])
+@router.get("/categories/", response_model=List[Category])
 def get_categories():
     conn = get_db_connection()
     cursor = conn.cursor()
+
     cursor.execute("SELECT id, name from categories")
     categories = cursor.fetchall()
     conn.close()
 
-    category_list = [{"id": cat[0],"name": cat[1]} for cat in categories]
+    category_list = [{"id": cat[0], "name": cat[1]} for cat in categories]
     return category_list
 
-@router.post("/categories/", response_model = Category)
-def create_categpru(category: CategoryCreate):
+@router.post("/categories/", response_model=Category)
+def create_category(category: CategoryCreate):
     conn = get_db_connection()
     cursor = conn.cursor()
 
@@ -33,7 +33,7 @@ def create_categpru(category: CategoryCreate):
         conn.close()
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            details=f"The category '{category.name}', already exists."
+            detail=f"The category '{category.name}', already exists."
         )
     except Exception as e:
         conn.close()
@@ -44,29 +44,28 @@ def create_categpru(category: CategoryCreate):
     finally:
         conn.close()
 
-@router.put("/categories/{category_id}",response_model=Category)
+@router.put('/categories/{category_id}', response_model=Category)
 def update_category(category_id: int, category: CategoryCreate):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("UPDATE categories SET name=? WHERE id=?", (category.name,category_id))
+    cursor.execute("UPDATE categories SET name = ? WHERE id = ?", (category.name, category_id))
     if cursor.rowcount == 0:
         conn.close()
         raise HTTPException(status_code=404, detail="Category not found")
     conn.commit()
     conn.close()
-    return Category(id = category_id, name=category.name)
+    return Category(id = category_id, name = category.name)
 
-@router.delete("/categories/{category_id}",response_model=dict)
+@router.delete("/categories/{category_id}", response_model=dict)
 def delete_category(category_id: int):
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM categories WHERE id=?",(category_id))
+    cursor.execute("DELETE FROM categories WHERE id = ? ", (category_id,))
     if cursor.rowcount == 0:
         conn.close()
         raise HTTPException(status_code=404, detail="Category not found")
     conn.commit()
     conn.close()
-    return {"details":"Category deleted"}
-
+    return {"details": "Category deleted"}
